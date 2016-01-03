ee8b430892081be91bd9ab48883a9646c3dbece6
TNTWZRD <tntwzrd.minecraft@gmail.com>
2016-01-03 03:40:46 +0000

    Styles Edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root 'root#index'
   
   get 'root/index' => 'root#index'
+  
+  get 'error/404' => 'error#404'
+  get 'error/422' => 'error#422'
+  get 'error/500' => 'error#500'
 
 end
